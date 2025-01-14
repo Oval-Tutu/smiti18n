98c05bb475af9130b48410b8a20e269fde889a85
Martin Wimpress <martin@wimpress.org>
2025-01-14 22:05:43 +0000

    feat: add polyglot gamedev translations

--- spec/en-UK.lua
+++ spec/en-UK.lua
@@ -1,50 +1,82 @@
 return {
   ["en-UK"] = {
     _formats = {
+      number = {
+        positive_symbol = "",
+        negative_symbol = "-",
+        decimal_symbol = ".",
+        thousand_separator = ",",
+        fract_digits = "2",
+      },
       currency = {
-        symbol = "£",
-        name = "British Pound",
         short_name = "GBP",
         decimal_symbol = ".",
         thousand_separator = ",",
-        fract_digits = 2,
+        fract_digits = "2",
         positive_symbol = "",
         negative_symbol = "-",
         positive_format = "%c %p%q",
-        negative_format = "%c %p%q"
-      },
-      number = {
-        decimal_symbol = ".",
-        thousand_separator = ",",
-        fract_digits = 2,
-        positive_symbol = "",
-        negative_symbol = "-"
-      },
-      date_time = {
-        long_time = "%g:%i:%s %a",
-        short_time = "%g:%i %a",
-        long_date = "%l %d %F %Y",
-        short_date = "%d/%m/%Y",
-        long_date_time = "%l %d %F %Y %g:%i:%s %a",
-        short_date_time = "%d/%m/%Y %g:%i %a"
-      },
-      short_month_names = {
-        "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep",
-        "oct", "nov", "dec"
-      },
-      long_month_names = {
-        "january", "february", "march", "april", "may", "jun", "july",
-        "august", "september", "october", "november", "december"
+        negative_format = "%c %p%q",
+        symbol = "£",
+        name = "British Pound",
       },
       short_day_names = {
-        "sun", "mon", "tue", "wed", "thu", "fri", "sat"
+        "Sun",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat"
+      },
+      date_time = {
+        short_date_time = "%d/%m/%Y %g:%i %a",
+        short_time = "%g:%i %a",
+        short_date = "%d/%m/%Y",
+        long_date_time = "%l %d %F %Y %g:%i:%s %a",
+        long_date = "%l %d %F %Y",
+        long_time = "%g:%i:%s %a",
       },
       long_day_names = {
-        "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"
-      }
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      },
+      short_month_names = {
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+      },
+      long_month_names = {
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "Jun",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      },
     },
-    hello = 'Hello!',
-    balance = 'Your account balance is %{value}.',
-    array = {"one", "two", "three"}
-  }
+    ["hello"] = 'Hello!',
+    ["balance"] = 'Your account balance is %{value}.',
+    ["array"] = {"one", "two", "three"},
+  },
 }
